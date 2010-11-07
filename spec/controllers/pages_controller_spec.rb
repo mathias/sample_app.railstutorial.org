89ceb051506fc28a7c5d468ac5ce1c359e490dc3
Matt Gauger <matt.gauger@gmail.com>
2010-11-07 02:10:50 -0600

    Forgot the render_views call, makes all the difference to pass tests.

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,7 +1,8 @@
 require 'spec_helper'
 
 describe PagesController do
-
+  render_views
+  
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
@@ -10,8 +11,7 @@ describe PagesController do
     
     it "should have the right title" do
       get 'home'
-      response.should have_selector("title",
-                      :content => "Ruby on Rails Tutorial Sample App | Home")
+      response.should have_selector('title', :content => 'Home')
     end
   end
 
@@ -38,6 +38,7 @@ describe PagesController do
       get 'about'
       response.should have_selector("title",
                       :content => "Ruby on Rails Tutorial Sample App | About")
+                      
     end
   end
   
