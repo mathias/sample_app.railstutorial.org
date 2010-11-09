6bc043fd9b330114caca52c1132f7a662f46887d
Matt Gauger <matt.gauger@gmail.com>
2010-11-08 22:58:42 -0600

    Add in @base_url to test per the RailsTutorial Exercise 3.5.2; all tests still pass.

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,6 +3,10 @@ require 'spec_helper'
 describe PagesController do
   render_views
   
+  before(:each) do
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
+  
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
@@ -11,7 +15,8 @@ describe PagesController do
     
     it "should have the right title" do
       get 'home'
-      response.should have_selector('title', :content => 'Home')
+      response.should have_selector('title', 
+                                    :content => @base_title + " | Home")
     end
   end
 
@@ -23,8 +28,8 @@ describe PagesController do
     
     it "should have the right title" do
       get 'contact'
-      response.should have_selector("title",
-                      :content => "Ruby on Rails Tutorial Sample App | Contact")
+      response.should have_selector("title", 
+                                    :content => @base_title + " | Contact")
     end
   end
 
@@ -36,8 +41,8 @@ describe PagesController do
     
     it "should have the right title" do
       get 'about'
-      response.should have_selector("title",
-                      :content => "Ruby on Rails Tutorial Sample App | About")
+      response.should have_selector("title", 
+                                    :content => @base_title + " | About")
     end
   end
   
@@ -50,7 +55,7 @@ describe PagesController do
     it "should have the right title" do
       get 'help'
       response.should have_selector("title",
-                      :content => "Ruby on Rails Tutorial Sample App | Help")
+                                    :content => @base_title + " | Help")
     end
   end
   
